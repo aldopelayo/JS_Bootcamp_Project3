593508972b593af4cee1980f27c4849cf2da728e
Aldo Pelayo <aldoorronaldo@gmail.com>
2022-05-14 22:40:24 -0400

    adding user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numeric = ["1","2","3","4","5","6","7","8","9","0"];
+var lowchar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upchar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var special = [",",".","/",";","'","[","]","-","=","!","@","#","$","%","^","&","*","(",")",];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,10 +14,32 @@ function writePassword() {
   passwordText.value = password;
 
 }
-var numbers = ["1","2","3","4","5","6","7","8","9","0"];
-var lowchar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upchar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var special = [",",".","/",";","'","[","]","-","=","!","@","#","$","%","^","&","*","(",")",];
+
+
+let passwordlength = window.prompt("choose length of password (8 to 124)",);
+var num1 = parseInt(passwordlength);
+console.log(passwordlength);
+//add an else here 
+numericChoice = confirm("would you like numbers to be included?")
+if (true) {
+    console.log(numericChoice)
+};
+
+charChoice1 = confirm("would you like lowercase letters to be included?")
+if (true) {
+  console.log(charChoice1)
+};
+
+charChoice2 = confirm("would you like Uppercase letters to be included?")
+if (true){
+  console.log(charChoice2)
+};
+
+specialChoice = confirm("would you like special characters to be included?")
+if(true) {
+  console.log(specialChoice)
+};
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
