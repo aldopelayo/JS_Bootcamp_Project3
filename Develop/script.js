02949525bc5f219fb11b8c0c13a53463105e9a12
Aldo Pelayo <aldoorronaldo@gmail.com>
2022-05-16 17:49:02 -0400

    PEER review

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //random character arrays
-    var numeric = ["1","2","3","4","5","6","7","8","9","0"];
-    var lowchar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    var upchar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    var special = [",",".","/",";","'","[","]","-","=","!","@","#","$","%","^","&","*","(",")"];
+    const numeric = ["1","2","3","4","5","6","7","8","9","0"];
+    const lowchar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    const upchar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    const special = [",",".","/",";","'","[","]","-","=","!","@","#","$","%","^","&","*","(",")"];
+    
+    let password= ""
+    let availableItems= ""
 //available items
 
 // Write password to the #password input
@@ -22,60 +25,54 @@ function writePassword() {
 
 
 function generatePassword (){
-  console.log("password generated");
 
+ 
     //questionaire
 let passwordlength = window.prompt("choose length of password (8 to 124)",);
-var num1 = parseInt(passwordlength);
+let num1 = parseInt(passwordlength);
 if (num1 > 128 ) {
   return "";
 } else if (num1 < 8) {
   return "";
 };
 if(num1 ===0) return;
-console.log(passwordlength);
-
-numericChoice = confirm("would you like numbers to be included?")
-if (true) {
-    console.log(numericChoice)
-};
-
-charChoice1 = confirm("would you like lowercase letters to be included?")
-if (true) {
-  console.log(charChoice1)
-};
-
-charChoice2 = confirm("would you like Uppercase letters to be included?")
-if (true){
-  console.log(charChoice2)
-};
-
-specialChoice = confirm("would you like special characters to be included?")
-if(true) {
-  console.log(specialChoice)
-};
-const Userinput = (num1,HasNumbers,HasUpper,HasLower,HasSpecial) =>{
-  const availableItems =[
-
-    ...(HasNumbers ? numeric : []),
-    ...(HasUpper ? upchar : []),
-    ...(HasLower ? lowchar: []),
-    ...(HasSpecial ? special: [])
-      
-  ];
 
 
+let numericChoice = confirm("would you like numbers to be included?");
 
-for(i=0;i<num1; i++){
-  const RandomValues = Math.floor(math.random()*availableItems.num1);
-  password+=availableItems[RandomValues];
+let charChoice1 = confirm("would you like lowercase letters to be included?");
+
+let charChoice2 = confirm("would you like Uppercase letters to be included?");
+
+let specialChoice = confirm("would you like special characters to be included?");
+
+
+var availableItems=[];
+if(numericChoice){
+  availableItems=numeric.concat();
 }
-console.log(availableItems);
-
-
-
+if(charChoice1){
+  availableItems=lowchar.concat(availableItems);
 }
-  return 
+if(charChoice2){
+  availableItems=upchar.concat(availableItems);
+}
+if(specialChoice){
+  availableItems=special.concat(availableItems);
+}
+
+let output = randomPassword();
+console.log(output)
+function randomPassword(){
+  var randomGenerate=''
+  for(i=0;i<num1; i++){
+  let RandomValues = Math.floor(Math.random()*availableItems.length);
+  randomGenerate+=availableItems[RandomValues];
+} return randomGenerate; }
+
+  return output;
+
+
 }
 
 
